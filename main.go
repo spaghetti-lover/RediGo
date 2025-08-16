d6becaa5475bb037437c1dc0d04a1ced7f8cc20f
spaghetti-lover <phungducanh2511@gmail.com>
2025-08-16 12:13:32 +0700

    feat: add thread pool

--- main.go
+++ main.go
@@ -6,12 +6,67 @@ import (
 	"time"
 )
 
+// element in the queue
+type Job struct {
+	conn net.Conn
+}
+
+// thread in the pool
+type Worker struct {
+	id       int
+	jobQueue chan Job
+}
+
+type Pool struct {
+	// queue
+	jobQueue chan Job
+	workers  []*Worker
+}
+
+func NewPool(n int) *Pool {
+	return &Pool{
+		jobQueue: make(chan Job),
+		workers:  make([]*Worker, n),
+	}
+}
+
+func (p *Pool) Start() {
+	for i := 0; i < len(p.workers); i++ {
+		worker := NewWorker(i, p.jobQueue)
+		p.workers[i] = worker
+		worker.Start()
+	}
+}
+
+func (p *Pool) AddJob(conn net.Conn) {
+	p.jobQueue <- Job{
+		conn: conn,
+	}
+}
+
+func NewWorker(id int, jobQueue chan Job) *Worker {
+	return &Worker{
+		id:       id,
+		jobQueue: jobQueue,
+	}
+}
+
+func (w *Worker) Start() {
+	go func() {
+		for job := range w.jobQueue {
+			log.Printf("worker %d is processing from %s", w.id, job.conn.RemoteAddr())
+			handleConnection(job.conn)
+		}
+	}()
+}
+
 // receive req and reply response
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	var buf []byte = make([]byte, 4096)
 
 	for {
+		// Blocking call
 		n, err := conn.Read(buf)
 
 		if err != nil {
@@ -24,6 +79,8 @@ func handleConnection(conn net.Conn) {
 
 		req := string(buf[:n])
 		log.Println("Received request: ", req)
+
+		// Pretend to receive request
 		time.Sleep(1 * time.Millisecond)
 		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello, world"))
 		if err != nil {
@@ -39,15 +96,16 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Println("Listening at port 3000")
+	defer listener.Close()
 
+	pool := NewPool(2)
+	pool.Start()
 	for {
 		// conn == socket == communication channel
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Println("handle conn from", conn.RemoteAddr())
-		go handleConnection(conn)
+		pool.AddJob(conn)
 	}
 }
